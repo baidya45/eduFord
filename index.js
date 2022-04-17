60bdb02d50c01f08b4719431798caceb0284f8aa
Baidyanath Dey <baidyadey11@gmail.com>
2022-04-17 23:08:47 +0530

    nav background added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 let menuBar = document.querySelector(".menu-bar");
 let navLinks = document.querySelector(".nav-links");
+let nav = document.querySelector('nav');
 
-function navToggle(){
+
 
     
-}
+window.addEventListener('scroll',()=>{
+    if(window.pageYOffset>650){
+        nav.classList.add('black');
+    }
+    else{
+        nav.classList.remove('black');
+    }
+})
 menuBar.addEventListener("click",()=>{
     navLinks.classList.toggle('active')
     menuBar.classList.toggle('menubartoggle')
 });
+
